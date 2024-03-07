95857a7182242c4061fe8f2a09a6885d3c693d88
Marek Gawryszewski <marek.gawryszewski@outlook.com>
2024-03-07 21:00:44 +0100

    Day 7/100
    Added Header component
    
    #16

--- src/ui/tubemanager-ui/src/app/app.component.ts
+++ src/ui/tubemanager-ui/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MoviesComponent } from './movies/movies.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
+    HeaderComponent,
     MoviesComponent
   ],
   templateUrl: './app.component.html',

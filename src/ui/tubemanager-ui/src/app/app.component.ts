a53283b122e80e0201c5a7ce49cec3b90dc87753
Marek Gawryszewski <marek.gawryszewski@outlook.com>
2024-03-07 21:02:02 +0100

    Day 7/100
    Added Footer component
    
    #17

--- src/ui/tubemanager-ui/src/app/app.component.ts
+++ src/ui/tubemanager-ui/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MoviesComponent } from './movies/movies.component';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +10,8 @@ import { HeaderComponent } from './components/header/header.component';
   imports: [
     RouterOutlet,
     HeaderComponent,
-    MoviesComponent
+    MoviesComponent,
+    FooterComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'

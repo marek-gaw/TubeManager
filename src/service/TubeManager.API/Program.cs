4e0b6d45ec6254aa93db273e15ee257f3ae2c3c8
Marek Gawryszewski <marek.gawryszewski@outlook.com>
2024-03-12 17:30:26 +0100

    Day 12/100
    Cleaned up Program.cs for API for clean start.
    
    #26

--- src/service/TubeManager.API/Program.cs
+++ src/service/TubeManager.API/Program.cs
@@ -1,24 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
 
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.MapGet("/helloworld", () =>
-{
-    return "Hello World";
-})
-.WithOpenApi();
+app.MapControllers();
 
 app.Run();

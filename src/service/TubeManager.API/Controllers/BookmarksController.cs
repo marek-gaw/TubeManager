29e8ce0bdaca8b70a4c0df7d139492817f1eae9f
Marek Gawryszewski <marek.gawryszewski@outlook.com>
2024-03-14 08:57:48 +0100

    Day 14/100
    Added initial implementation of POST /bookmarks
    
    #29

--- src/service/TubeManager.API/Controllers/BookmarksController.cs
+++ src/service/TubeManager.API/Controllers/BookmarksController.cs
@@ -1,10 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
+using TubeManager.API.Models;
 
 namespace TubeManager.API.Controllers;
 
 [Route("bookmarks")]
 public class BookmarksController : ControllerBase
 {
+    // not thread safe.
+    private static readonly List<Bookmark> _bookmarks = new();
+    private static Guid _id = Guid.NewGuid();
+    
     [HttpGet]
     public void Get()
     {
@@ -12,9 +17,23 @@ public class BookmarksController : ControllerBase
     }
 
     [HttpPost]
-    public void Post()
+    public void Post([FromBody] Bookmark bookmark)
     {
+        bookmark.Id = _id;
+
+        var bookmarkExists = _bookmarks
+            .Any(b => b.VideoUrl == bookmark.VideoUrl);
+
+        if (bookmarkExists)
+        {
+            HttpContext.Response.StatusCode = StatusCodes.Status406NotAcceptable;
+            return;
+        }
         
+        _bookmarks.Add(bookmark);
+        HttpContext.Response.StatusCode = StatusCodes.Status201Created;
+        HttpContext.Response.Headers.Add("Location", $"bookmarks/{_id}");
+        _id = Guid.NewGuid();
     }
 
     [HttpPut]

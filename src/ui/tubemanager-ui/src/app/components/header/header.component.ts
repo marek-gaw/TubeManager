ab5223902d4fa5b5f76c631407fa14688e61404f
Marek Gawryszewski <marek.gawryszewski@outlook.com>
2024-04-10 19:39:28 +0200

    Day 40/100 - UI
    Added indicator to the nav bar to show current page.

--- src/ui/tubemanager-ui/src/app/components/header/header.component.ts
+++ src/ui/tubemanager-ui/src/app/components/header/header.component.ts
@@ -1,13 +1,61 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
 
+  recentSelected: boolean = false
+  bookmarksSelected: boolean = false
+  importSelected: boolean = false
+  authorSelected: boolean = false
+
+  selectedNavItem: string[] = [
+    'recent', 'bookmarks', 'import', 'author'
+  ]
+
+  onSelect(element: string): void {
+    switch(element){
+      case 'recent': {
+        this.recentSelected = true
+        this.bookmarksSelected = false
+        this.importSelected = false
+        this.authorSelected = false
+        break;
+      }
+      case 'bookmarks': {
+        this.recentSelected = false
+        this.bookmarksSelected = true
+        this.importSelected = false
+        this.authorSelected = false
+        break;
+      }
+      case 'import': {
+        this.recentSelected = false
+        this.bookmarksSelected = false
+        this.importSelected = true
+        this.authorSelected = false
+        break;
+      }
+      case 'author': {
+        this.recentSelected = false
+        this.bookmarksSelected = false
+        this.importSelected = false
+        this.authorSelected = true
+        break;
+      }
+      default: {
+        this.recentSelected = false
+        this.bookmarksSelected = false
+        this.importSelected = false
+        this.authorSelected = false
+      }
+    }
+  }
 }

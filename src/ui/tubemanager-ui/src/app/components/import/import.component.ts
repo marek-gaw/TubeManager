6469d49da8d047cf4a65509472cdef88dd5d7e2d
Marek Gawryszewski <marek.gawryszewski@outlook.com>
2024-04-22 19:47:36 +0200

    Day 53/100 ui
    Archive import: file is sent to the APi.

--- src/ui/tubemanager-ui/src/app/components/import/import.component.ts
+++ src/ui/tubemanager-ui/src/app/components/import/import.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClient, HttpEventType } from '@angular/common/http';
+import { HttpClient, HttpEventType, HttpRequest, HttpResponse } from '@angular/common/http';
 import { NgIf } from '@angular/common';
-import { Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-import',
   standalone: true,
   imports: [
     FormsModule,
-    NgIf
+    NgIf,
   ],
   templateUrl: './import.component.html',
   styleUrl: './import.component.css'
@@ -21,6 +21,7 @@ export class ImportComponent {
   fileName = '';
   uploadProgress: number = 0;
   uploadSub: Subscription = Subscription.EMPTY;
+  importStatus: string = "";
 
   constructor(private http: HttpClient) {
     this.requiredFileType = ".skytube";
@@ -33,17 +34,26 @@ export class ImportComponent {
   onFileSelected(event: any) {
     const file: File = event.target.files[0];
 
+    console.log(event.target.files[0]);
+
     if (file) {
 
       this.fileName = file.name;
 
       const formData = new FormData();
 
-      formData.append("thumbnail", file);
+      formData.append("file", file);
+      formData.append("Content-Type", "multipart/form-data; boundary=WebAppBoundary");
+      formData.append("Content-Disposition", "form-data; name=\"file\"; filename=\"${file.name}\"");
 
-      const upload$ = this.http.post("/api/thumbnail-upload", formData);
+      const upload$ = this.http.post("http://localhost:5126/import", formData, { observe: 'response' });
 
-      upload$.subscribe();
+      upload$.subscribe((res) => {
+        if (res.status == 200) {
+          console.log(res);
+          this.importStatus = res.statusText;
+        }
+      });
     }
   }
 }

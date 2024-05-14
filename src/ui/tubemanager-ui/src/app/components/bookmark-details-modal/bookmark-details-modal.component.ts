30a3d39ce2b51a66bedf98a643958124a6aa0cd4
Marek Gawryszewski <marek.gawryszewski@outlook.com>
2024-05-14 11:23:34 +0200

    Day 75/100 ui
    Dropdown to select/add existing tag to a bookmark

--- src/ui/tubemanager-ui/src/app/components/bookmark-details-modal/bookmark-details-modal.component.ts
+++ src/ui/tubemanager-ui/src/app/components/bookmark-details-modal/bookmark-details-modal.component.ts
@@ -1,13 +1,16 @@
 import { Component, Input, inject } from '@angular/core';
-import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { NgbActiveModal, NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 import { Bookmark } from '../../interfaces/bookmark';
 import { NgFor } from '@angular/common';
+import { Tags } from '../../interfaces/tags';
+import { TagsService } from '../../services/tags.service';
 
 @Component({
   selector: 'app-bookmark-details-modal',
   standalone: true,
   imports: [
-    NgFor
+    NgFor,
+    NgbDropdownModule
   ],
   templateUrl: './bookmark-details-modal.component.html',
   styleUrl: './bookmark-details-modal.component.css'
@@ -15,4 +18,30 @@ import { NgFor } from '@angular/common';
 export class BookmarkDetailsModalComponent {
   activeModal = inject(NgbActiveModal);
   @Input() bookmark?: Bookmark;
+  tags: Tags[] = [];
+
+  constructor(private tagsService: TagsService) { }
+
+  ngOnInit(): void {
+
+  }
+
+  onShowDropdown(): void {
+    console.log(`Dropdown opened`)
+    this.tagsService.getAll().subscribe(data => {
+      this.tags = data;
+      console.log(data);
+    })
+  }
+
+  onKeyUp(ev: KeyboardEvent): void {
+    console.log(`event:${ev}`);
+    var value = ev.key;
+
+  }
+
+  onDropdownPosClicked(tag: Tags): void {
+    console.log(`tag: ${tag.id} | ${tag.title}`);
+    this.bookmark?.tags.push(tag);
+  }
 }

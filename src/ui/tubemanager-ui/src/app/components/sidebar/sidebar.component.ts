dd1db4b276de4db533e40f729819d6fca5cc6fa6
Marek Gawryszewski <marek.gawryszewski@outlook.com>
2024-05-17 15:32:30 +0200

    Day 78/100 ui
    Fixed routing, added links to sidebar

--- src/ui/tubemanager-ui/src/app/components/sidebar/sidebar.component.ts
+++ src/ui/tubemanager-ui/src/app/components/sidebar/sidebar.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
+
+constructor(route: ActivatedRoute){
+  route.params.subscribe(params => console.log("side menu id parameter",params['id']));
 }
+}
+

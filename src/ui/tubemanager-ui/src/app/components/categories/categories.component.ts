ce6df42bcd9145cc239bcd2c41b13a303c082753
Marek Gawryszewski <marek.gawryszewski@outlook.com>
2024-05-20 15:24:47 +0200

    Day 81/100 ui
    Added Categories component

---
+++ src/ui/tubemanager-ui/src/app/components/categories/categories.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-categories',
+  standalone: true,
+  imports: [],
+  templateUrl: './categories.component.html',
+  styleUrl: './categories.component.css'
+})
+export class CategoriesComponent {
+
+}

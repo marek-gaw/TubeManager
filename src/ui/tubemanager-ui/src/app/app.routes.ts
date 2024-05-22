153b14a3af5aab99ec1a488479bb55e7a408de2e
Marek Gawryszewski <marek.gawryszewski@outlook.com>
2024-05-22 16:29:11 +0200

    Day 83/100 ui
    Added route to Channels component and option in sidebar

--- src/ui/tubemanager-ui/src/app/app.routes.ts
+++ src/ui/tubemanager-ui/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { AlbumComponent } from './components/album/album.component';
 import { ImportComponent } from './components/import/import.component';
 import { TagsComponent } from './components/tags/tags.component';
+import { ChannelsComponent } from './components/channels/channels.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'bookmarks', pathMatch: 'full' },
     { path: 'bookmarks', component: AlbumComponent },
     { path: 'import', component: ImportComponent},
-    { path: 'tags', component: TagsComponent}
+    { path: 'tags', component: TagsComponent},
+    { path: 'channels', component: ChannelsComponent}
 ];

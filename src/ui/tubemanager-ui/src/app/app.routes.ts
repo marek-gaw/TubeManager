005930bcb775ecadb63815582b31eafc40a77de9
Marek Gawryszewski <marek.gawryszewski@outlook.com>
2024-11-03 10:16:20 +0100

    Fixed main page: all bookmarks shown by default. Added sidebar option to add new bookmark

--- src/ui/tubemanager-ui/src/app/app.routes.ts
+++ src/ui/tubemanager-ui/src/app/app.routes.ts
@@ -4,10 +4,12 @@ import { TagsComponent } from './components/tags/tags.component';
 import { ChannelsComponent } from './components/channels/channels.component';
 import { CategoriesComponent } from './components/categories/categories.component';
 import { NewBookmarkComponent } from './components/new-bookmark/new-bookmark.component';
+import { AlbumComponent } from './components/album/album.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'bookmarks', pathMatch: 'full' },
-    { path: 'bookmarks', component: NewBookmarkComponent },
+    { path: 'bookmarks', component: AlbumComponent },
+    { path: 'new-bookmark', component: NewBookmarkComponent },
     { path: 'import', component: ImportComponent },
     { path: 'tags', component: TagsComponent },
     { path: 'channels', component: ChannelsComponent },
